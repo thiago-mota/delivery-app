ecb515f9e9c284f81639f77645892037ba893d1f
Rubio <paulocezarrubio@gmail.com>
2023-01-20 18:32:50 -0300

    <chore>(migrations)

--- back-end/src/router/routerLogin.js
+++ back-end/src/router/routerLogin.js
@@ -1,8 +1,8 @@
-// const express = require('express');
-// const controllerUser = require('../controllers/controllerUser')
+const express = require('express');
+const controllerUser = require('../controllers/controllerUser')
 
-// const routers = express.Router();
+const routers = express.Router();
 
-// routers.post('/', controllerUser.postLogin)
+routers.post('/', controllerUser.postLogin)
 
-// module.exports = routers;
+module.exports = routers;
