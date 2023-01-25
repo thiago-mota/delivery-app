8ca16714125b03f41b7388518f66984536aaa440
DimyLeo <leonardoprimoc@hotmail.com>
2023-01-25 16:09:59 -0300

    Add get e delete users para gerenciamento de adm

--- back-end/src/router/router.js
+++ back-end/src/router/router.js
@@ -1,13 +1,13 @@
 const express = require('express');
 // const { authorizationToken } = require('../middlewares/authorizationToken');
 const routeLogin = require('./routerLogin');
-const routeRegister = require('./routerUser');
+const routeUsers = require('./routerUser');
 const routeProducts = require('./routerProducts');
 
 const routers = express.Router();
 
 routers.use('/login', routeLogin);
-routers.use('/register', routeRegister);
+routers.use('/users', routeUsers);
 routers.use('/products', routeProducts);
 
 // routers.use(authorizationToken);
