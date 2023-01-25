324f4aab25d4ce200626a6fc19e3490720bf7ad6
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-01-25 17:47:16 -0300

    feat(routes): added /costumer/products route

---
+++ front-end/src/pages/CustomerProducts/CustomerProducts.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// [
+//   {
+//     id: 1,
+//     name: 'Skol Lata 250ml',
+//     price: 2.20,
+//     url_image: 'http://localhost:3001/images/skol_lata_350ml.jpg',
+//   },
+// ]
+
+const CustomerProducts = () => {
+  return (
+    <div>
+      Hello World!
+    </div>
+  );
+};
+
+export default CustomerProducts;
