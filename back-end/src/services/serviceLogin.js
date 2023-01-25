19f8d6beb03ec7718cb9127c2d331802300e77f1
Thiago Mota <83622639+thiago-mota@users.noreply.github.com>
2023-01-25 09:14:29 -0300

    Update serviceLogin.js

--- back-end/src/services/serviceLogin.js
+++ back-end/src/services/serviceLogin.js
@@ -3,7 +3,6 @@ const md5 = require('md5');
 const { User } = require('../database/models/index');
 const ErrorGenerator = require('../utils/errorGenerator');
 
-
 const jwtConfig = {
   expiresIn: '20D',
   algorithm: 'HS256',
@@ -19,21 +18,21 @@ const findUserbyEmail = async ({ email }) => User.findOne({
   where: { email },
 });
 
-const isBadRequest = ({email, password}) => {
+const isBadRequest = ({ email, password }) => {
   const isValidEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/gi;
   return isValidEmail.test(email) && password.length > 6;
-}
+};
 
 const loginService = async ({ email, password }) => {
   const requestType = isBadRequest({ email, password });
   
-  if (!requestType) throw new ErrorGenerator(403, "bad request")
+  if (!requestType) throw new ErrorGenerator(403, 'bad request');
   
   const user = await findUserbyEmail({ email });
-  if (!user) throw new ErrorGenerator(404, "user not found");
+  if (!user) throw new ErrorGenerator(404, 'user not found');
    
   const newPassword = md5(password);
-  if (user.password !== newPassword) throw new ErrorGenerator(401, "wrong password");
+  if (user.password !== newPassword) throw new ErrorGenerator(401, 'wrong password');
   const checkUser = await existenceUser({ email, password: newPassword });
   const response = { 
     name: checkUser.name, email: checkUser.email, role: checkUser.role,
