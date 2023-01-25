f982d25305a828fcb4ddc8198b3faa25f7eba6c8
Marinavdac <marinavdac@gmail.com>
2023-01-25 09:01:25 -0300

    feat: cria validações e erro para rota de login

---
+++ back-end/src/middlewares/errorMiddleware.js
@@ -0,0 +1,10 @@
+module.exports = (err, _req, res, _next) => {
+  if (err.status) {
+    return res.status(err.status).json({ message: err.message });
+  }
+  return res.status(500).json({
+    error: {
+      message: "Internal Server Error",
+    },
+  });
+};
