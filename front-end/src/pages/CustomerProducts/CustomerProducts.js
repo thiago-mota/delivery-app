14ca62fe0c9389dfa1306d353284f81bf9746d09
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2023-01-27 19:09:01 -0300

    chore: linting

--- front-end/src/pages/CustomerProducts/CustomerProducts.js
+++ front-end/src/pages/CustomerProducts/CustomerProducts.js
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import CheckoutButton from '../../components/CheckoutButton/CheckoutButton';
 import Header from '../../components/Header/Header';
 import Product from '../../components/Product/Product';
@@ -11,23 +10,29 @@ import styles from './CustomerProducts.module.css';
 // ZW1haWwiOiJjbGllbnRlQGhvdG1haWwuY29tIiwicm9sZSI6ImN1c3RvbWVyIiwiaWF0IjoxNjc0NjgwNjUwLCJleH
 // AiOjE2NzY0MDg2NTB9.68xRy7oZC4RnwFBc0CSBxDBm3roI1h8l_OmAsckxxw4`;
 
-// duvida1 posso usar o useEffect no customHook useFetch?
-// duvida2 posso passar os options assim \/ ?
-const fetchOptions = ({
-  method: 'get',
-  url: 'http://localhost:3001/products',
-  headers: { Authorization: getLocalStorage('user')?.token },
-});
+// duvida posso passar os options assim para meu usehook \/, se fizer assim ele não roda a função
+// get localstorage toda re-renderização ? conversando com o Tiago Paz ele falou que preferiria usando
+// um useMemo ou useRef mesmo
+// const fetchOptions = {
+//   method: 'get',
+//   url: 'http://localhost:3001/products',
+//   headers: { Authorization: getLocalStorage('user')?.token },
+// };
 
 function CustomerProducts() {
-  // const fetchOptions = useRef();
+  // const [count, setCount] = useState(0);
+  const fetchOptions = useRef({
+    method: 'get',
+    url: 'http://localhost:3001/products',
+    headers: { Authorization: getLocalStorage('user')?.token },
+  });
 
-  const [count, setCount] = useState(0);
   const [data, isLoading] = useFetch(fetchOptions);
   const products = data?.data;
   return (
     <>
-      <button type='button' onClick={ () => setCount((prev) => (prev + 1)) }> { count } </button>
+      {/* Dev button para testes, pfv ignorar \/  */}
+      {/* <button type='button' onClick={ () => setCount((prev) => (prev + 1)) }> { count } </button> */}
       <Header />
       <div className={styles['page-container']}>
         <div className={styles.container}>
